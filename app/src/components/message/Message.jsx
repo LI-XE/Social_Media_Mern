6eb6cf08fdaff7a7563738d16657863c5079617e
LI_XE <hyangeun312@gmail.com>
2022-08-26 03:06:35 -0500

    updated messenger

--- app/src/components/message/Message.jsx
+++ app/src/components/message/Message.jsx
@@ -1,12 +1,30 @@
 import "./message.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { format } from "timeago.js";
+import axios from "axios";
 
 function Message({ message, own }) {
+  const [user, setUser] = useState({});
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      await axios.get(`/user?userId=${message.sender}`).then((res) => {
+        console.log(res);
+        setUser(res.data);
+      });
+    };
+    fetchUser();
+  }, [message]);
+
   return (
     <div className={own ? "message own" : "message"}>
       <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo6PXZJ1aARRNQzPGSl05mcyD-259xRyejUw&usqp=CAU"
+        src={
+          user.profilePicture
+            ? PF + user.profilePicture
+            : PF + "profile/noAvatar.png"
+        }
         alt=""
         className="messageImg"
       />
